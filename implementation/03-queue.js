e6765bfc579bb3f1ff83c247dc5b11fc184a53c6
gasser <gasserkhaled23@gmail.com>
2023-08-03 14:21:11 +0300

    compelete the implementation of queue

--- implementation/03-queue.js
+++ implementation/03-queue.js
@@ -1,27 +1,44 @@
 const { SinglyLinkedNode } = require("./01-singly-linked-list");
 
 class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
 
-    constructor() {
-        this.head = null;
-        this.tail = null;
+  enqueue(val) {
+    // Add node to end of queue (linked list)
+    const newNode = new SinglyLinkedNode(val);
+
+    if (!this.head) {
+      this.head = this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
+    this.length++;
+    return this.length;
+    // Write your hypothesis on the time complexity of this method here
+    // Big-O(1)
+  }
 
-    enqueue(val) {
-        // Add node to end of queue (linked list)
-
-        // Write your hypothesis on the time complexity of this method here
+  dequeue() {
+    // Remove node from front of queue (linked list)
+    if (this.head) {
+      let data = this.head.value;
+      if (!this.head.next) this.tail = null;
+      this.head = this.head.next;
+      this.length--;
+      return data;
     }
-
-    dequeue() {
-        // Remove node from front of queue (linked list)
-        
-        // Write your hypothesis on the time complexity of this method here
-    }
-
+    return null;
+    // Write your hypothesis on the time complexity of this method here
+    // Big-O(1)
+  }
 }
 
 module.exports = {
-    Queue,
-    SinglyLinkedNode
-}
+  Queue,
+  SinglyLinkedNode,
+};
